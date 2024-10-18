5894038a52a18995899ff845f870bc7957a4a2ec
BusterNoren08 <buster.noren@elev.ga.lbs.se>
2024-10-18 15:08:36 +0200

    new game

--- My project/Assets/scripts/EnemyMovement.cs
+++ My project/Assets/scripts/EnemyMovement.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy1movement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] Transform player;
     [SerializeField] float EnemySpeed = 2f;
     Rigidbody2D rb;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
     void Update()
     {
-        Vector2 direction = (player.position - transform.position);
-        rb.MovePosition(rb.S + direction * EnemySpeed);
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.MovePosition(rb.position + direction * EnemySpeed * Time.fixedDeltaTime);
     }
 }
