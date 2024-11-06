650d3862c973b73c8793e8188e72ec67e3a2d8e1
BusterNoren08 <buster.noren@elev.ga.lbs.se>
2024-11-06 21:08:33 +0100

    .
    topdown shooter projet

--- My project/Assets/scripts/PlayerMovement.cs
+++ My project/Assets/scripts/PlayerMovement.cs
@@ -6,9 +6,11 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Vector2 screenBoundery;
     Rigidbody2D rb;
     [SerializeField] float moveSpeed = 5f;
-    
+    [SerializeField] float rotationSpeed = 2000f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,6 +24,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.velocity = moveInput * moveSpeed;
-        Debug.Log(moveInput);
+        if(moveInput != Vector2.zero)
+        {
+            Debug.Log(moveInput);
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, moveInput);
+            Quaternion rotate = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            rb.MoveRotation(rotate);
+        }
+        else
+        {
+            rb.angularVelocity = 0;
+        }
+        screenBoundery = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -screenBoundery.x, screenBoundery.x), Mathf.Clamp(transform.position.y,-screenBoundery.y, screenBoundery.y));
     }
 }
